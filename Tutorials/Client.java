ad947586fa18da20b73098729bb0d41d77c6d286
Sepehr <sepehrmsm1379@gmail.com>
2020-08-12 23:34:55 +0430

    simple server client that they can chat together with java

---
+++ Tutorials/Client.java
@@ -0,0 +1,80 @@
+package Tutorials;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        ClientImp client = new ClientImp("localhost", 7777);
+
+        try {
+            Thread t = new Thread(()->{
+                //reading thread
+                new Thread(()->{
+                   while (true){
+                       String text = client.getMessage();
+
+                       System.out.println(text);
+                   }
+                }).start();
+
+                //writing thread
+                new Thread(()->{
+                    Scanner scanner = new Scanner(System.in);
+                   while(true){
+                       String input = scanner.nextLine();
+
+                       client.sentMessage(input);
+                   }
+                }).start();
+            });
+        t.start();
+        t.join();
+        } catch (InterruptedException e) {
+            System.err.println("cant join");
+        }
+
+    }
+}
+class ClientImp{
+
+    private Socket socket;
+    private DataInputStream dataInputStream;
+    private DataOutputStream dataOutputStream;
+
+    public ClientImp(String host, int port){
+        System.out.println("Trying to connect server...");
+        try {
+            socket = new Socket("localhost", port);
+            dataInputStream = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+            dataOutputStream = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+            System.out.println("Successfully connected");
+        } catch (Exception e) {
+            System.err.println("Client cannot connect to server!");
+        }
+    }
+
+    public void sentMessage(String message){
+        try {
+            dataOutputStream.writeUTF(message);
+            dataOutputStream.flush();
+        } catch (IOException e) {
+            System.out.println("Cannot send message!");
+        }
+    }
+    public String getMessage(){
+
+        String text = "";
+        while (text.equals("")){
+            try {
+                text = dataInputStream.readUTF();
+            } catch (Exception e) {
+
+            }
+        }
+
+        return text;
+    }
+
+}
---
+++ Tutorials/Server.java
@@ -0,0 +1,88 @@
+package Tutorials;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Server {
+    public static void main(String[] args) {
+        ServerImp server = new ServerImp();
+
+        server.serverOn(7777);
+
+        try {
+            Thread t = new Thread(()-> {
+                //reading thread
+                new Thread(() -> {
+                    while (true) {
+                        try {
+                            String text = server.getMessage();
+                            System.out.println(text);
+                        } catch (Exception e) {
+
+                        }
+                    }
+                }).start();
+
+                //writing thread
+                new Thread(() -> {
+                    Scanner scanner = new Scanner(System.in);
+                    while (true) {
+                        String text = scanner.nextLine();
+
+                        server.sendMessage(text);
+                    }
+                }).start();
+            });
+            t.start();
+            t.join();
+        } catch (InterruptedException e) {
+            System.err.println("cant join");
+        }
+
+    }
+
+}
+class ServerImp{
+
+    private ServerSocket serverSocket = null;
+    private DataInputStream dataInputStream = null;
+    private DataOutputStream dataOutputStream = null;
+    private Socket clientSocket = null;
+
+    public ServerImp(){
+
+    }
+
+    public void serverOn(int port){
+        System.out.println("Waiting for client to connect...");
+
+        try {
+            serverSocket = new ServerSocket(port);
+            clientSocket = serverSocket.accept();
+            dataInputStream = new DataInputStream(new BufferedInputStream(clientSocket.getInputStream()));
+            dataOutputStream = new DataOutputStream(new BufferedOutputStream(clientSocket.getOutputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Client successfully connected!");
+    }
+    public void sendMessage(String text){
+        try {
+            dataOutputStream.writeUTF(text);
+            dataOutputStream.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public String getMessage() throws IOException {
+        return dataInputStream.readUTF();
+    }
+    public boolean isClientConnected(){
+        if(clientSocket != null)
+            return true;
+        return false;
+    }
+}
